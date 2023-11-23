f1c88fac121b1e0c80b69dcd5135f749c7d06343
MarcoGranata9 <granatam9900@gmail.com>
2023-11-23 14:46:27 +0100

    import components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
+import AppHeader from "./components/AppHeader.vue"
+import AppMain from "./components/AppMain.vue"
+import AppFooter from "./components/AppFooter.vue"
 
+export default {
+    components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+},
+}
 </script>
 
 <template>
- 
+<AppHeader/>
 </template>
 
 <style lang="scss">
 @use "./style/general.scss";
+
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+img {
+    max-width: 100%;
+}
+
+body {
+    font-family: Arial, Helvetica, sans-serif;
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,19 @@
 <script>
 export default {
-    
+    data() {
+        return {
+            
+        }
+    }
 }
 </script>
 
 <template>
-
+    <header>
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+    </header>
 </template>
 
 <style scoped lang="scss">
